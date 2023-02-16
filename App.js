a517fe168b66292e9c9ab5451ee72827c7298a3b
AnnZoeFreelancer <annzoefreelancer@gmail.com>
2023-02-16 17:25:24 +0530

    NR-Ch-03-LayingTheFoundation

--- App.js
+++ App.js
@@ -1,40 +1,33 @@
 import React from "react";
 import { createRoot, ReactDOM } from "react-dom/client";
 
-//Create a Nested header element (h1,h2,h3 inside a div with class="title")
-//using React.createElement
-const heading = React.createElement("h1",{className:"topic"},"Using React.createElement");
-const heading1 = React.createElement("h1",{className:"title"},"Heading 1");
-const heading2 = React.createElement("h2",{className:"title"},"Heading 2");
-const heading3 = React.createElement("h3",{className:"title"},"Heading3");
-const nestedReactCreateElement = React.createElement("div",{id:"nestedCreateElement"},[heading,heading1,heading2,heading3]);
-
-//using JSX
-const header=<h1 className="topic"> Using JSX</h1>
-const header1=<h1 className="title">Heading 1</h1>;
-const header2=<h2 className="title">Heading 2</h2>
-const header3=<h3 className="title">Heading 3</h3>
-
-//using FunctionalComponent
-const HeaderComponent = ()=>{
+const HeaderComponent = () =>{
     return (
-   <div>
-    
-    <h1 className="topic">Using Functional Component</h1>
-    <h1 className="title">Heading 1</h1>
-    <h2 className="title">Heading 2</h2>
-    <h3 className="title">Heading 3</h3>
-    <Header/>
-    <Header/>
-   </div>   
-)};
+        <>
+            <div className="navbar">
+                <Logo/><SearchBar/><UserIcon/>
+            </div>
+        </>
+    )
+}
+const Logo = ()=>{
+    return (
+            <image className="logo" src="/assignment/logo.jpg">Image</image>
+    )
+}
+const SearchBar=()=>{
+    return(
+            <input  className="searchBar" type="text" placeholder="Searchbar"></input>
 
-//Using Composition Component - Component inside component
-const Header=()=>{
-    return <h3 className="topic">Using Component Composition</h3>;
+    )
+}
+const UserIcon=()=>{
+    return(
+        <image className="userIcon" src="userIcon.png">UserIcon</image>
+    )
 }
 
-const container = React.createElement("div",{id:"container"},[nestedReactCreateElement, header, header1,header2,header3,<HeaderComponent/>]);
+const container = React.createElement("div",{id:"container"},[<HeaderComponent/>]);
 
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 const root= createRoot(document.getElementById("root"));
